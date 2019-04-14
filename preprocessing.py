6acf3c4582f57af6109aabfd71c5e30cadd52b2b
gohyinhao <yinhao.g@gmail.com>
2019-04-15 01:17:15 +0800

    Refactor preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,43 +1,59 @@
 import pandas as pd
 import numpy as np
 
+# class to preprocess data given CSV file containing stock/index prices.
+# breaks up data into sets of {@param: seq_len} prices and the next day price (target output).
+
 
 class DataProcessing:
-    def __init__(self, file, train):
+    def __init__(self, file, percentage_as_training):
         self.file = pd.read_csv(file)
-        self.train = train
-        self.i = int(self.train * len(self.file))
-        self.stock_train = self.file[0: self.i]
-        self.stock_test = self.file[self.i:]
-        self.input_train = []
-        self.output_train = []
-        self.input_test = []
-        self.output_test = []
+        self.percentage_as_training = percentage_as_training
+        self.index = int(self.percentage_as_training * len(self.file))
+        self.training_set = self.file[0: self.index]
+        self.validation_set = self.file[self.index:]
+        self.training_input = []
+        self.training_output = []
+        self.validation_input = []
+        self.validation_output = []
 
-    def gen_train(self, seq_len):
+    # split data set into training set based on percentage_as_training
+    def generate_training_set(self, seq_len):
         """
         Generates training data
         :param seq_len: length of window
-        :return: X_train and Y_train
+        :return: X_training and Y_training
         """
-        for i in range((len(self.stock_train)//seq_len)*seq_len - seq_len - 1):
-            x = np.array(self.stock_train.iloc[i: i + seq_len, 1])
-            y = np.array([self.stock_train.iloc[i + seq_len + 1, 1]], np.float64)
-            self.input_train.append(x)
-            self.output_train.append(y)
-        self.X_train = np.array(self.input_train)
-        self.Y_train = np.array(self.output_train)
 
-    def gen_test(self, seq_len):
+        # represents zero-based column number in CSV file
+        COLUMN_NUMBER = 1
+
+        for i in range((len(self.training_set)//seq_len)*seq_len - seq_len - 1):
+            x = np.array(self.training_set.iloc[i: i + seq_len, COLUMN_NUMBER])
+            y = np.array(
+                [self.training_set.iloc[i + seq_len + 1, COLUMN_NUMBER]], np.float64)
+            self.training_input.append(x)
+            self.training_output.append(y)
+        self.X_training = np.array(self.training_input)
+        self.Y_training = np.array(self.training_output)
+
+    # split data set into validation set based on percentage_as_training
+    def generate_validation_set(self, seq_len):
         """
-        Generates test data
+        Generates validation data
         :param seq_len: Length of window
         :return: X_test and Y_test
         """
-        for i in range((len(self.stock_test)//seq_len)*seq_len - seq_len - 1):
-            x = np.array(self.stock_test.iloc[i: i + seq_len, 1])
-            y = np.array([self.stock_test.iloc[i + seq_len + 1, 1]], np.float64)
-            self.input_test.append(x)
-            self.output_test.append(y)
-        self.X_test = np.array(self.input_test)
-        self.Y_test = np.array(self.output_test)
+
+        # represents zero-based column number in CSV file
+        COLUMN_NUMBER = 1
+
+        for i in range((len(self.validation_set)//seq_len)*seq_len - seq_len - 1):
+            x = np.array(
+                self.validation_set.iloc[i: i + seq_len, COLUMN_NUMBER])
+            y = np.array(
+                [self.validation_set.iloc[i + seq_len + 1, COLUMN_NUMBER]], np.float64)
+            self.validation_input.append(x)
+            self.validation_output.append(y)
+        self.X_validation = np.array(self.validation_input)
+        self.Y_validation = np.array(self.validation_output)
