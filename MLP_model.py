ee07314553761e45f153471d4a7dbc2270832870
gohyinhao <yinhao.g@gmail.com>
2019-04-17 02:12:36 +0800

    Fix errors in MLP model

--- MLP_model.py
+++ MLP_model.py
@@ -1,34 +1,70 @@
-import get_prices as hist
+import pandas as pd
+import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import quandl as quandl
+import keras
 from preprocessing import DataProcessing
-# import pandas_datareader.data as pdr if using the single test below
-import fix_yahoo_finance as fix
-fix.pdr_override()
 
-start = "2003-01-01"
-end = "2018-01-01"
+# live data extraction
+TICKER = "WGC/GOLD_DAILY_USD"
+data = quandl.get("WGC/GOLD_DAILY_USD", authtoken="FUw5PPY7UX5AW-svaskC")
+data = data[37:]  # before index 37, prices were not daily
 
-hist.get_stock_data("AAPL", start_date=start, end_date=end)
-process = DataProcessing("stock_prices.csv", 0.9)
-process.generate_validation_set(10)
-process.generate_training_set(10)
+# optimizable variables
+PERCENTAGE_OF_DATA_FOR_TRAINING = 0.8
+SEQ_LENGTH = 10
+N_DAY_FORECAST = 1
 
-X_training = process.X_training / 200
-Y_training = process.Y_training / 200
+data_processor = DataProcessing(
+    data, PERCENTAGE_OF_DATA_FOR_TRAINING, N_DAY_FORECAST)
+data_processor.generate_validation_set(SEQ_LENGTH)
+data_processor.generate_training_set(SEQ_LENGTH)
 
-X_validation = process.X_validation / 200
-Y_validation = process.Y_validation / 200
+# typically normalize by substracting average and dividing by standard deviation
+# but we want to use the output and check against live trade data thus not entirely feasible
+# hence simply divide by a constant to ensure weights in network do not become too large
+NORMALIZATION_CONSTANT = 1000
+
+X_training = data_processor.X_training / NORMALIZATION_CONSTANT
+Y_training = data_processor.Y_training / NORMALIZATION_CONSTANT
+
+X_validation = data_processor.X_validation / NORMALIZATION_CONSTANT
+Y_validation = data_processor.Y_validation / NORMALIZATION_CONSTANT
+
+# MLP with 2 hidden layers
+# consider changing number of hidden layers as well as number of nodes
+# uses Rectified Linear Unit (ReLU) activation function
+NUM_OF_NODES = 100
+NUM_OF_OUTPUT = 1
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Dense(100, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(100, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(1, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(NUM_OF_NODES, activation=tf.nn.leaky_relu))
+model.add(tf.keras.layers.Dense(NUM_OF_NODES, activation=tf.nn.leaky_relu))
+model.add(tf.keras.layers.Dense(NUM_OF_OUTPUT, activation=tf.nn.leaky_relu))
 
+# adam optimizer used in lieu of traditional stochastic gradient descent.
+# consider possibility of looking at other optimizers
 model.compile(optimizer="adam", loss="mean_squared_error")
 
-model.fit(X_training, Y_training, epochs=100)
+# fit model with our training data
+es = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=50)
+model.fit(X_training, Y_training, epochs=500, callbacks=[es])
 
-print(model.evaluate(X_validation, Y_validation))
+# get un-normalized data
+X_actual = X_validation * NORMALIZATION_CONSTANT
+Y_actual = Y_validation * NORMALIZATION_CONSTANT
+Y_prediction = model.predict(X_validation) * NORMALIZATION_CONSTANT
+
+# plot prediction vs. actual
+plt.plot(Y_prediction, color='red', label='Predicted Gold Price')
+plt.plot(Y_actual, color='blue', label='Actual Gold Price')
+plt.title('Gold 1-day Price Forecast (MLP)')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.legend()
+plt.show()
+tf.keras.backend.clear_session()
 
 # If instead of a full backtest, you just want to see how accurate the model is for a particular prediction, run this:
 # data = pdr.get_data_yahoo("AAPL", "2017-12-19", "2018-01-03")
